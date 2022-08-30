df4df58108590ca6e7fbdd7568910e378b12bdc1
tunji <Oladipupoolatunji272@gmail.com>
2022-08-31 00:17:34 +0100

    Initial Commit

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_NEW_URL", "sqlite:///cafes.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("sqlite:///cafes.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 engine = create_engine('sqlite:///cafes.db', convert_unicode=True, echo=False)
 Base = declarative_base()
