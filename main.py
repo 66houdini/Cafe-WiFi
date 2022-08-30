7bd6cc19443aded3954b7c415aaa9442d2e53bb0
tunji <Oladipupoolatunji272@gmail.com>
2022-08-30 20:36:15 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'oasismontesorrischool'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///cafes.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+engine = create_engine('sqlite:///cafes.db', convert_unicode=True, echo=False)
+Base = declarative_base()
+Base.metadata.reflect(engine)
+db = SQLAlchemy(app)
+Bootstrap(app)
+db.Model.metadata.reflect(db.engine)
+
+
+class CafeList(db.Model):
+    __table__ = db.Model.metadata.tables["cafe"]
+
+    def __repr__(self):
+        return self.name
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/cafes")
+def cafes_list():
+    cafes = CafeList.query.all()
+    return render_template("cafes.html", cafes=cafes)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
